2cdb3d6dba14890044cb598e61e4c728a9993e76
http-riscila <prigadelha10@gmail.com>
2025-01-28 18:10:49 -0300

    feat:mudancaDePontuacao

--- src/components/pedidos/FinalPedido.jsx
+++ src/components/pedidos/FinalPedido.jsx
@@ -6,7 +6,7 @@ export default function FinalPedido() {
     return (
         <div className="pedido-final-container">
             <div className="pedido-final-content">
-                <h2>Pedido finalizado com sucesso!</h2>
+                <h2>Pedido finalizado com sucesso.</h2>
                 <p>Obrigada por comprar conosco!</p>
                 <Link to={'/'}><button type="button" className="pedido-final-button">Voltar para a página inicial</button></Link>
             </div>
